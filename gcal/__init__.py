85e3374239feb4694e7f1977818866ef6b8788c0
Nurul-GC <nuruldecarvalho@gmail.com>
2022-01-08 17:44:56 +0200

    atualizado gcal

---
+++ gcal/__init__.py
@@ -0,0 +1,63 @@
+﻿"""
+(c) 2019-2021 Nurul-GC
+"""
+
+from configparser import ConfigParser
+import os
+import webbrowser
+from random import randint
+from sys import argv, exit
+from time import sleep
+
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
+
+from gcal.gui import GCal
+
+
+class Gcal:
+    def __init__(self):
+        self.gc = QApplication(argv)
+
+
+        for fontfile in fonte:
+            QFontDatabase.addApplicationFont(fontfile)
+
+            img = QPixmap("img/favicons/favicon-512x512.png")
+            self.align = int(
+                Qt.AlignmentFlag.AlignBottom | Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignAbsolute)
+            self.janela = QSplashScreen(img)
+            self.janela.setStyleSheet(tema)
+            self.janela.show()
+            self.iniciar()
+
+    def iniciar(self):
+        load = 0
+        while load < 100:
+            self.janela.showMessage(f"Carregando os Modulos do Programa: {load}%", self.align, Qt.GlobalColor.black)
+            load += randint(1, 10)
+            sleep(0.5)
+        self.janela.close()
+        GCal().ferramentas.show()
+        GCal().ferramentas.setStyleSheet(tema)
+
+
+
+if __name__ == '__main__':
+    inifile = ConfigParser()
+    if os.path.exists('gcalculadora.ini'):
+        inifile.read('gcalculadora.ini')
+        if inifile['MAIN']['tema'] == 'Escuro':
+            tema = open('themes/gcalculadora.qss').read().strip()
+        else:
+            tema = open('themes/gcalculadora-light.qss').read().strip()
+    else:
+        inifile['MAIN'] = {'tema': 'Claro', 'imagem': 'img/2.png'}
+        with open('gcalculadora.ini', 'w') as INIFILE:
+            inifile.write(INIFILE)
+        tema = open('themes/gcalculadora-light.qss').read().strip()
+    fonte = [f'fonts/{fontFile}' for fontFile in os.listdir('fonts') if fontFile.endswith('.ttf')]
+
+    app = Gcal()
+    app.gc.exec_()
